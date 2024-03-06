80114b997310e57df99ba7aa400f7fa9ba75c222
sahil-chhoker <chhokersahil435@gmail.com>
2024-03-06 22:08:15 +0530

    moved the templates forms

--- src/products/views.py
+++ src/products/views.py
@@ -7,4 +7,4 @@ def product_detail_view(request):
     context={
         'object': obj
     }
-    return render(request, "product/detail.html", {})
+    return render(request, "products/product_detail.html", {})
