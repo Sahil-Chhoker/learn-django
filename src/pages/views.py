ce4c25e7dc0175181fe55662127ed88337836c2c
sahil-chhoker <chhokersahil435@gmail.com>
2024-03-06 21:35:51 +0530

    defferent template tags and filters

--- src/pages/views.py
+++ src/pages/views.py
@@ -10,10 +10,11 @@ def contact_view(request, *args, **kwargs):
 
 def about_view(request, *args, **kwargs):
     my_context = {
-        "my_text": "This is about us",
+        "title": "This is about us",
         "this_is_true": True,
         "my_number": 123,
-        "my_list": [123, 4242, 12313]
+        "my_list": [123, 4242, 12313],
+        "my_html": "<h1>Hello World</h1>"
     }
     return render(request, "about.html", my_context)
 
