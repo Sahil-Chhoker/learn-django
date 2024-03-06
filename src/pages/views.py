eef5725a5a6c55a3940404bb9b228fe86c8da17c
sahil-chhoker <chhokersahil435@gmail.com>
2024-03-06 19:52:12 +0530

    applied templates to all page

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Hellp World</h1>") # string of HTML code
     return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+    return render(request, "contact.html", {})
